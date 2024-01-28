ac749dd9ae1a57ca470348174c70ae4104515416
Jorge Alcala <jorge1alcala@msn.com>
2024-01-27 17:32:29 -0800

    modal start

---
+++ components/CustomButton.tsx
@@ -0,0 +1,34 @@
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+function CustomButton({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+}: CustomButtonProps) {
+  return (
+    <button
+      className={`custom-btn ${containerStyles}`}
+      disabled={false}
+      type={btnType || "button"}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="arrow_left"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
+    </button>
+  );
+}
+
+export default CustomButton;
---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+type Props = {
+  title: string;
+};
+
+function CustomFilter({ title }: Props) {
+  return <div>Custom Filter</div>;
+}
+
+export default CustomFilter;
